dedc656bca6ba549817ded5d719b92afd5bd1eac
Lin <1725525653@qq.com>
2022-07-08 19:21:18 +0800

    �

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,37 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template, request
 import api.manufacturer as manufacturer
 import api.manufacturer_detection_box as manufacturer_detection_box
 import api.qr_code as qr_code
 import api.upload as upload
 import api.watermark as watermark
+from flasgger import Swagger
+
+from config.setting import DESCRIPTION, ROUTE, TERMSOFSERVICE, TITLE, VERSION
 
 app = Flask(__name__)
+print(app.static_folder)
 app.config["JSON_AS_ASCII"] = False  # jsonify返回的中文正常显示
+
+
+swagger_config = Swagger.DEFAULT_CONFIG
+swagger_config['title'] = TITLE
+swagger_config['version'] = VERSION
+swagger_config['termsOfService'] = TERMSOFSERVICE
+swagger_config['specs'][0]['route'] = ROUTE
+swagger_config['description'] = DESCRIPTION
+
+Swagger(app, config=swagger_config)
+
+
+"""
+协议说明
+"""
+
+
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+
 
 """
 下面为生产厂商相关路由注册
@@ -15,31 +40,113 @@ app.config["JSON_AS_ASCII"] = False  # jsonify返回的中文正常显示
 
 @app.route(manufacturer.baseUrl + "/addManufacturer", methods=["POST"])
 def add_manufacturer():
+    """
+    添加生产商
+    ---
+    tags:
+      - 生产商接口
+    responses:
+      1001:
+        description: 不可传入空
+      1002:
+        description: 该厂商已经存在
+      1003:
+        description: 该厂商前缀已经存在
+      0:
+        description: 恭喜，添加成功
+    """
     return manufacturer.add_manufacturer()
 
 
 @app.route(manufacturer.baseUrl + "/manufacturerDelete/<string:id>", methods=["DELETE"])
 def manufacturer_delete(id):
+    """
+    通过删除生产商
+    ---
+    tags:
+      - 生产商接口
+    responses:
+      1001:
+        description: 不可传入空
+      1004:
+        description: 查不到相关厂商的信息
+      0:
+        description: 查询成功
+    """
     return manufacturer.manufacturer_delete(id)
 
 
 @app.route(manufacturer.baseUrl + "/manufacturerUpdate/<string:id>", methods=["PUT"])
 def manufacturer_update(id):
+    """
+    通过ID更新生产商
+    ---
+    tags:
+      - 生产商接口
+    responses:
+      1001:
+        description: 不可传入空
+      1002:
+        description: 该厂商已经存在
+      0:
+        description: 恭喜，修改生产厂商信息成功
+    """
     return manufacturer.manufacturer_update(id)
 
 
 @app.route(manufacturer.baseUrl + "/getAll", methods=["GET"])
 def get_all_manufacturer():
+    """
+    获取所有生产商
+    ---
+    tags:
+      - 生产商接口
+    responses:
+      1001:
+        description: 不可传入空
+      1002:
+        description: 该厂商已经存在
+      1003:
+        description: 该厂商前缀已经存在
+      0:
+        description: 查询成功
+    """
     return manufacturer.get_all_manufacturer()
 
 
 @app.route(manufacturer.baseUrl + "/getManufacturerById/<string:id>", methods=["GET"])
 def get_manufacturer_by_id(id):
+    """
+    通过ID获取生产商
+    ---
+    tags:
+      - 生产商接口
+    responses:
+      1001:
+        description: 不可传入空
+      1004:
+        description: 查不到相关厂商的信息
+      0:
+        description: 查询成功
+    """
     return manufacturer.get_manufacturer_by_id(id)
 
 
 @app.route(manufacturer.baseUrl + "/getManufacturerByFactoryName/<string:factory_name>", methods=["GET"])
 def get_manufacturer_by_factory_name(factory_name):
+    """
+    通过名字获取生产商
+    ---
+    tags:
+      - 生产商接口
+    responses:
+      1001:
+        description: 不可传入空
+      1004:
+        description: 查不到相关厂商的信息
+      0:
+        description: 恭喜，删除检测盒信息成功
+    """
     return manufacturer.get_manufacturer_by_factory_name(factory_name)
 
 
@@ -50,26 +157,87 @@ def get_manufacturer_by_factory_name(factory_name):
 
 @app.route(manufacturer_detection_box.baseUrl + "/getAll", methods=["GET"])
 def get_all_manufacturer_detection_box():
+    """
+    获取所有自检盒
+    ---
+    tags:
+      - 自检盒接口
+    responses:
+      0:
+        description: 查询成功
+    """
     return manufacturer_detection_box.get_all_manufacturer_detection_box()
 
 
 @app.route(manufacturer_detection_box.baseUrl + "/manufacturerDetectionBoxDelete/<string:id>", methods=["DELETE"])
 def manufacturer_detection_box_delete(id):
+    """
+    通过id删除自检盒
+    ---
+    tags:
+      - 自检盒接口
+    responses:
+      1001:
+        description: 不可传入空
+      1004:
+        description: 查不到相关检测盒的信息
+      0:
+        description: 查询成功
+    """
     return manufacturer_detection_box.manufacturer_detection_box_delete(id)
 
 
 @app.route(manufacturer_detection_box.baseUrl + "/getManufacturerDetectionBoxById/<string:id>", methods=["GET"])
 def get_manufacturer_detection_box_by_id(id):
+    """
+    通过ID获取自检盒
+    ---
+    tags:
+      - 自检盒接口
+    responses:
+      1001:
+        description: 不可传入空
+      1004:
+        description: 查不到相关检测盒的信息
+      0:
+        description: 查询成功
+    """
     return manufacturer_detection_box.get_manufacturer_detection_box_by_id(id)
 
 
 @app.route(manufacturer_detection_box.baseUrl + "/addManufacturerDetectionBox", methods=["POST"])
 def add_manufacturer_detection_box():
+    """
+    添加自检盒
+    ---
+    tags:
+      - 自检盒接口
+    responses:
+      1001:
+        description: 不可传入空
+      1004:
+        description: 查不到相关检测盒的信息
+      0:
+        description: 恭喜，添加成功
+    """
     return manufacturer_detection_box.add_manufacturer_detection_box()
 
 
 @app.route(manufacturer_detection_box.baseUrl+"/isValidProductionSerialNumber/<string:production_serial_number>", methods=["POST"])
 def is_valid_production_serial_number(production_serial_number):
+    """
+    判断是否有效
+    ---
+    tags:
+      - 自检盒接口
+    responses:
+      1001:
+        description: 不可传入空
+      1004:
+        description: 查不到相关检测盒的信息
+      0:
+        description: 检验合法
+    """
     return manufacturer_detection_box.is_valid_production_serial_number(production_serial_number)
 
 
@@ -78,14 +246,31 @@ def is_valid_production_serial_number(production_serial_number):
 """
 
 
-@app.route("/generateQRcode/<string:my_str>", methods=["POST"])
+@app.route("/api/generateQRcode/<string:my_str>", methods=["GET"])
 def generate_qrcode(my_str):
+    """
+    生成二维码
+    ---
+    tags:
+      - 二维码接口
+    responses:
+      0:
+        description: 生成成功
+    """
     return qr_code.generate_qrcode(my_str)
 
 
-@app.route("/images/<image_id>.jpg", methods=["GET"])
+@app.route("/api/images/<image_id>.jpg", methods=["GET"])
 def get_qrcode(image_id):
-    """查看二维码"""
+    """
+    查看二维码
+    ---
+    tags:
+      - 二维码接口
+    responses:
+      0:
+        description: 查看成功
+    """
     return qr_code.get_qrcode(image_id)
 
 
@@ -94,8 +279,19 @@ def get_qrcode(image_id):
 """
 
 
-@app.route("/photo/upload", methods=['POST', "GET"])
+@app.route("/api/photo/upload", methods=['POST', "GET"])
 def uploads():
+    """
+    上传图片
+    ---
+    tags:
+      - 上传图片接口
+    responses:
+      1005:
+        description: 格式错误,请上传jpg格式文件
+      0:
+        description: 上传成功
+    """
     return upload.uploads()
 
 
@@ -106,9 +302,27 @@ def uploads():
 
 @app.route(watermark.baseUrl+"/watermarkEmbed/<string:filePath>/<string:watermarkInfo>", methods=["POST"])
 def watermark_embed(filePath, watermarkInfo):
+    """
+    水印嵌入
+    ---
+    tags:
+      - 水印接口
+    responses:
+      0:
+        description: 水印嵌入成功
+    """
     return watermark.watermark_embed(filePath, watermarkInfo)
 
 
 @app.route(watermark.baseUrl+"/watermarkExtract/<string:filePath>", methods=["POST"])
 def watermark_extract(filePath):
+    """
+    水印提取
+    ---
+    tags:
+      - 水印接口
+    responses:
+      0:
+        description: 水印提取成功
+    """
     return watermark.watermark_extract(filePath)
--- api/manufacturer.py
+++ api/manufacturer.py
@@ -2,7 +2,7 @@ from common.mysql_operate import db
 from flask import jsonify, request
 import uuid
 
-baseUrl = "/manufacturer"
+baseUrl = "/api/manufacturer"
 
 
 def get_all_manufacturer():
@@ -18,17 +18,20 @@ def add_manufacturer():
     factory_prefix = request.json.get("factory_prefix", "").strip()  # 厂商前缀
 
     if factory_name and factory_prefix:  # 注意if条件中 "" 也是空, 按False处理
-        sql1 = "SELECT * FROM manufacturer WHERE factory_name = '{}'".format(factory_name)
+        sql1 = "SELECT * FROM manufacturer WHERE factory_name = '{}'".format(
+            factory_name)
         data = db.select_db(sql1)
         if data:
             return jsonify({"code": 1002, "msg": "该厂商已经存在"})
-        sql2 = "SELECT * FROM manufacturer WHERE factory_prefix = '{}'".format(factory_prefix)
+        sql2 = "SELECT * FROM manufacturer WHERE factory_prefix = '{}'".format(
+            factory_prefix)
         data = db.select_db(sql2)
         if data:
             return jsonify({"code": 1003, "msg": "该厂商前缀已经存在"})
         id = uuid.uuid1()  # 厂商的唯一id
         sql3 = "INSERT INTO manufacturer(id,factory_name,factory_prefix) " \
-               "VALUES('{}', '{}', '{}')".format(id, factory_name, factory_prefix)
+               "VALUES('{}', '{}', '{}')".format(
+                   id, factory_name, factory_prefix)
         db.execute_db(sql3)
         print("新增生产厂商信息SQL ==>> {}".format(sql2))
         return jsonify({"code": 0, "msg": "恭喜，添加成功！"})
@@ -39,12 +42,13 @@ def add_manufacturer():
 def get_manufacturer_by_factory_name(factory_name):
     if factory_name:
         """获取某个生产厂商信息"""
-        sql = "SELECT * FROM manufacturer WHERE factory_name = '{}'".format(factory_name)
+        sql = "SELECT * FROM manufacturer WHERE factory_name = '{}'".format(
+            factory_name)
         data = db.select_db(sql)
         print("获取 {} 生产厂商信息 == >> {}".format(factory_name, data))
         if data:
             return jsonify({"code": 0, "data": data, "msg": "查询成功"})
-        return jsonify({"code": "1004", "msg": "查不到相关厂商的信息"})
+        return jsonify({"code": 1004, "msg": "查不到相关厂商的信息"})
     return jsonify({"code": 1001, "msg": "不可传入空"})
 
 
@@ -56,7 +60,7 @@ def get_manufacturer_by_id(id):
         print("获取 {} 生产厂商信息 == >> {}".format(id, data))
         if data:
             return jsonify({"code": 0, "data": data, "msg": "查询成功"})
-        return jsonify({"code": "1004", "msg": "查不到相关厂商的信息"})
+        return jsonify({"code": 1004, "msg": "查不到相关厂商的信息"})
     return jsonify({"code": 1001, "msg": "不可传入空"})
 
 
@@ -67,7 +71,8 @@ def manufacturer_update(id):  # id为准备修改的生产厂商ID
 
     if id and factory_name and factory_prefix:  # 注意if条件中空串 "" 也是空, 按False处理
         """首先查看修改后的名字是否已经在数据库中"""
-        sql = "SELECT * FROM manufacturer WHERE factory_name = '{}'".format(factory_name)
+        sql = "SELECT * FROM manufacturer WHERE factory_name = '{}'".format(
+            factory_name)
         data = db.select_db(sql)
         if data:
             return jsonify({"code": 1002, "msg": "该厂商已经存在"})
@@ -89,5 +94,5 @@ def manufacturer_delete(id):
             db.execute_db(sql2)
             print("删除生产厂商信息SQL ==>> {}".format(sql2))
             return jsonify({"code": 0, "msg": "恭喜，删除生产厂商信息成功！"})
-        return jsonify({"code": "1004", "msg": "查不到相关厂商的信息"})
+        return jsonify({"code": 1004, "msg": "查不到相关厂商的信息"})
     return jsonify({"code": 1001, "msg": "不可传入空"})
